84e6e90704957e9b09f821a5e6b2536b15328dce
joaquin gaona <joaquinngaona@gmail.com>
2020-09-03 10:36:01 -0300

    diseno

--- front/src/screens/home/Home.jsx
+++ front/src/screens/home/Home.jsx
@@ -11,72 +11,58 @@ import {
   Typography,
   Avatar,
 } from "@material-ui/core";
+import "./style.css";
 
-const Home = ({ articles }) => {
+const Home = ({ articles, firstArticle }) => {
   return (
-    <Grid
-      spacing={1}
-      container
-      direction="row"
-      justify="space-evenly"
-      alignItems="flex-start"
-    >
-      {articles &&
-        articles.map((article) => {
-          return (
-            <Grid container item xs={12} sm={4} spacing={1}>
-              <Link to={`/article/${article.articleURL}`}>
-                <Card>
-                  <CardActionArea>
-                    <CardMedia
-                      component="img"
-                      alt="IMAGEN"
-                      height="140"
-                      image="https://neilpatel.com/wp-content/uploads/2017/08/blog.jpg"
-                      title="Articulo blog"
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="h2">
-                        {article.articleTitle}
-                      </Typography>
+    <>
+      <div>
+        <h3>{firstArticle && firstArticle.articleTitle}</h3>
+      </div>
+      <div className="card-container">
+        {articles &&
+          articles.map((article) => {
+            return (
+              <div className="card">
+                <Link
+                  to={`/article/${article.articleURL}`}
+                  style={{ textDecoration: "none" }}
+                >
+                  <img
+                    className="cardImage"
+                    src={article.imageArticle}
+                    alt="foto"
+                  />
 
-                      <Grid
-                        spacing={1}
-                        container
-                        direction="row"
-                        justify="flex-start"
-                        alignItems="flex-start"
-                      >
-                        {article.articleAuthor.map((author) => {
-                          return (
-                            <Grid container item xs={6} spacing={1}>
-                              <Avatar
-                                alt="Remy Sharp"
-                                src="https://media.gettyimages.com/photos/punk-portrait-picture-id939681086?s=612x612" //{author.profilePicture}
-                              />
-                              <Typography>
-                                {author.firstName} {author.lastName}
-                              </Typography>
-                            </Grid>
-                          );
-                        })}
-                      </Grid>
+                  <div className="card-content">
+                    <h3>{article.articleTitle}</h3>
 
-                      <Typography
-                        variant="body2"
-                        color="textSecondary"
-                        component="p"
-                      >
-                        {article.articleDescription}
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                </Card>
-              </Link>
-            </Grid>
-          );
-        })}
-    </Grid>
+                    <div className="authors-container">
+                      {article.articleAuthor.map((author) => {
+                        return (
+                          <div className="author">
+                            <img
+                              className="avatar"
+                              src="https://media.gettyimages.com/photos/punk-portrait-picture-id939681086?s=612x612"
+                              alt="foto"
+                            />
+
+                            <p className="author-name">
+                              {author.firstName} {author.lastName}
+                            </p>
+                          </div>
+                        );
+                      })}
+                    </div>
+
+                    <p className="cardInfo">{article.articleDescription}</p>
+                  </div>
+                </Link>
+              </div>
+            );
+          })}
+      </div>
+    </>
   );
 };
 
