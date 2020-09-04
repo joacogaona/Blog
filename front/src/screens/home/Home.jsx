9463d2ac8c4c0b2ed7297f0057a6d1522d608cfc
joaquin gaona <joaquinngaona@gmail.com>
2020-09-04 17:40:05 -0300

    diseno home listo

--- front/src/screens/home/Home.jsx
+++ front/src/screens/home/Home.jsx
@@ -16,44 +16,50 @@ import "./style.css";
 const Home = ({ articles, firstArticle }) => {
   return (
     <>
-      <div className="first-article">
-        {/*  <Link
-          to={`/article/${firstArticle.articleURL}`}
-          style={{ textDecoration: "none" }}
-        > */}
+      <Link
+        to={`/article/${firstArticle && firstArticle.articleURL}`}
+        style={{ color: "inherit", textDecoration: "inherit" }}
+      >
+        <div className="first-article">
+          <div className="first-article-content">
+            <div className="first-article-content-container">
+              <div className="first-title">
+                <h2 style={{ color: "#FFD600" }}>ÚLTIMO TEMA</h2>
+                <h2>DEL BLOG</h2>
+              </div>
+              <h5>
+                {firstArticle && firstArticle.readingTime} minutos de lectura
+              </h5>
+              <h3>{firstArticle && firstArticle.articleTitle}</h3>
 
-        <div className="first-article-content">
-          <div className="first-title">
-            <h2>ÚLTIMO TEMA DEL BLOG</h2>
+              <p className="first-article-info">
+                {firstArticle && firstArticle.articleDescription}
+              </p>
+
+              <div className="first-article-authors-container">
+                {firstArticle &&
+                  firstArticle.articleAuthor.map((author) => {
+                    return (
+                      <div className="first-article-author">
+                        <p className="first-article-author-name">
+                          {author.firstName} {author.lastName}
+                        </p>
+                        <p className="first-article-author-job">
+                          {author.userJob}
+                        </p>
+                      </div>
+                    );
+                  })}
+              </div>
+            </div>
           </div>
-          <h5>{firstArticle && firstArticle.readingTime} minutos de lectura</h5>
-          <h3>{firstArticle && firstArticle.articleTitle}</h3>
-
-          <p className="first-article-info">
-            {firstArticle && firstArticle.articleDescription}
-          </p>
-
-          <div className="first-article-authors-container">
-            {firstArticle &&
-              firstArticle.articleAuthor.map((author) => {
-                return (
-                  <div className="first-article-author">
-                    <p className="first-article-author-name">
-                      {author.firstName} {author.lastName}
-                    </p>
-                    <p className="first-article-author-job">{author.userJob}</p>
-                  </div>
-                );
-              })}
-          </div>
+          <img
+            className="first-article-image"
+            src={firstArticle && firstArticle.imageArticle}
+            alt="foto"
+          />
         </div>
-        <img
-          className="first-article-image"
-          src={firstArticle && firstArticle.imageArticle}
-          alt="foto"
-        />
-        {/*  </Link> */}
-      </div>
+      </Link>
       {/* ------------------------------------------------------------------------------------------------- */}
       <div className="card-container">
         {articles &&
@@ -82,8 +88,8 @@ const Home = ({ articles, firstArticle }) => {
                               src="https://media.gettyimages.com/photos/punk-portrait-picture-id939681086?s=612x612"
                               alt="foto"
                             />
-                            <div>
-                              <p className="author-name">
+                            <div className="author-name">
+                              <p>
                                 {author.firstName} {author.lastName}
                               </p>
                             </div>
